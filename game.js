549003c74b6526a35d0f19db8c8fba93150372ca
Matt Campbell <matt@mattcampbell.net>
2012-08-25 15:15:18 -0400

    Adding agile ship

--- game.js
+++ game.js
@@ -276,6 +276,31 @@
 		ctx.fill();
 	};
 
+	function AgileShip() {}
+	AgileShip.prototype = new DoubleLaserShip();
+	AgileShip.prototype.moveLeft = function (delta) {
+		this.x -= this.vx * delta * 0.15;
+
+		if (this.x < -this.width) {
+			this.x += this.env.width + this.width;
+		}
+	};
+	AgileShip.prototype.moveRight = function (delta) {
+		this.x += this.vx * delta * 0.15;
+		this.x %= this.env.width;
+	};
+	AgileShip.prototype.moveUp = function (delta) {
+		this.y -= this.vy * delta * 0.2;
+
+		if (this.y < -this.height) {
+			this.y += this.env.height + this.height;
+		}
+	};
+	AgileShip.prototype.moveDown = function (delta) {
+		this.y += this.vy * delta * 0.2;
+		this.y %= this.env.height;
+	};
+
 	game = (function () {
 		var width = 800,
 			height = 600,
@@ -287,7 +312,7 @@
 
 		function init() {
 			starfield = new Starfield(width, height);
-			ship = new DoubleLaserShip();
+			ship = new AgileShip();
 			ship.init(this);
 		}
 
